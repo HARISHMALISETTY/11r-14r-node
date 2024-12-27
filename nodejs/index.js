0bfe20e17034772e530fdff9424c044382938c53
HARISHMALISETTY <malisettyharish@gmail.com>
2024-12-27 15:11:32 +0530

    middlewares

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,23 +1,15 @@
-const http = require("http");
 const express = require("express");
 const exp = require("constants");
 const app = express();
 app.use(express.json());
-app.get('/hello',(req,res)=>{
-    let data=[{ name: "harish", city: "hyd" }]
-    res.send(data)
+const { userNameValidator, passwordValidator } = require("./middlewares.js");
+
+app.post("/signup", userNameValidator, passwordValidator, (req, res) => {
+  let data = req.body;
+
+  res.send(data);
 });
-app.post("/data",(req,res)=>{
-    const data=req.query;
-    console.log(JSON.stringify(data));
-    res.send(data)
-})
-app.put("/u*e",(req,res)=>{
-    console.log(req.body);
-    res.send(req.body)
-})
+
 app.listen(3100, () => {
   console.log("server is running");
 });
-
-
