acdd69438bc5e962aa5503e7ddaed531410a3c62
HARISHMALISETTY <malisettyharish@gmail.com>
2025-01-07 21:47:47 +0530

    multer-intro

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,12 +5,37 @@ app.use(express.json());
 const port = process.env.HTTP_PORT;
 const { dbConnect } = require("./db.js");
 const bcrypt = require("bcrypt");
-const userRoutes=require("./routes/userRoutes.js")
+const userRoutes = require("./routes/userRoutes.js");
+const path = require("path");
+const fs = require("fs");
+
+console.log(__dirname);
+
+let newPath = path.join(__dirname, "uploads");
+
+if (!fs.existsSync(newPath)) {
+  fs.mkdirSync(newPath);
+}
+
+// function createSampleFolder() {
+//   let newPath = path.join(__dirname, "sample");
+//   fs.mkdirSync(newPath);
+// }
+
+// createSampleFolder()
+// console.log(newPath);
+
+// let base=path.basename(__dirname)
+
+// let file="normal.jpg";
+
+// console.log(path.extname(file))
+
+// console.log(base)
 
 dbConnect();
 
-
-app.use('/', userRoutes);
+app.use("/", userRoutes);
 
 app.listen(port, () => {
   console.log("server is running");
