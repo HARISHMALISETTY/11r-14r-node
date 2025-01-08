10de00f5afa0e0a1f9be50094fbee78198bc63ce
HARISHMALISETTY <malisettyharish@gmail.com>
2025-01-08 16:31:15 +0530

    multer

--- nodejs/index.js
+++ nodejs/index.js
@@ -8,6 +8,10 @@ const bcrypt = require("bcrypt");
 const userRoutes = require("./routes/userRoutes.js");
 const path = require("path");
 const fs = require("fs");
+const multer=require("multer");
+const jwt=require("jsonwebtoken");
+const jwt_secret_key = "hjdcvfknjdvnfk";
+
 
 console.log(__dirname);
 
@@ -17,26 +21,30 @@ if (!fs.existsSync(newPath)) {
   fs.mkdirSync(newPath);
 }
 
-// function createSampleFolder() {
-//   let newPath = path.join(__dirname, "sample");
-//   fs.mkdirSync(newPath);
-// }
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, newPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-// createSampleFolder()
-// console.log(newPath);
-
-// let base=path.basename(__dirname)
-
-// let file="normal.jpg";
-
-// console.log(path.extname(file))
-
-// console.log(base)
+const upload = multer({ storage: storage });
 
 dbConnect();
 
 app.use("/", userRoutes);
 
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log("Request received");
+  console.log("Uploaded File:", req.file);
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  res.status(201).send({ file: req.file });
+});
+
 app.listen(port, () => {
-  console.log("server is running");
+  console.log("server is running", port);
 });
